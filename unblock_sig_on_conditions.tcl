c09230eeceee0feda1e7e5778b704ffa24cee323
sh33pdog <keiron.shepherd@gmail.com>
2024-12-18 22:33:56 +0000

    Update  logging unblock_sig_on_conditions.tcl

--- unblock_sig_on_conditions.tcl
+++ unblock_sig_on_conditions.tcl
@@ -1,9 +1,8 @@
 when ASM_REQUEST_DONE {
-    # Check if the signature ID 200001493 is present, the HTTP path is "user/register",
-    # and the Authorization header exists
+    # Check if the signature ID 200001493 is present, the HTTP path is "user/register" and the Authorization header exists
     if { ([ASM::violation details] contains "sig_data.sig_id 200001493") && ([string tolower [HTTP::path]] eq "/user/register") && [HTTP::header exists "Authorization"] } {
         # Log details about the unblocked request
-        log local0. "Request unblocked - URI: [HTTP::uri] - ClientIP: [IP::client_addr] - Authorization: [HTTP::header Authorization]"
+        log local0. "Request unblocked to avoid false positive on ASM violation for XSS signature ID 200001493 for encoded + in bearer token - URI: [HTTP::uri] - ClientIP: [IP::client_addr] - Authorization: [HTTP::header Authorization]"
         
         # Unblock the request
         ASM::unblock
